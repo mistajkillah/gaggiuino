cd022c9ce071de67b68e6ea6385a6c268a8c9016
John Jackson <john.jackson@mirageinfinite>
2023-10-31 21:06:25 -0400

    more stuff

--- playground/Drivers/Spi.cpp
+++ playground/Drivers/Spi.cpp
@@ -1,29 +1,4 @@
-/*
- * Spi.cpp
- *
- * SPI Byte / Bit order:
- *   Note SPI sends data MSB first.  So we represent data as a byte string with
- *   the first byte (or leftmost byte looking at it as a string) is shifted first
- *   MSB of each byte first as well.  In other words, this is BIG ENDIAN.
- *
- *   For transactions that are other than a multiple of 8 bits, the last byte in the
- *   byte array is truncated as needed.
- *
- *  Created on: Aug 18, 2016
- *      Author: reichert
- */
 
-
- /* Endianess.
-  *   myUnion.asByte[0] == 0x03 ? "BIG" : "LITTLE",
-  *   For Big Endian machines (PPC), the 0th byte of an integer is (or a 4 byte byte array!)
-  *   is the Most Significant byte, which is what we want.
-  *
-  *   For little endian machines (Intel, Arm) the 0th byte of the byte array is
-  *   the least significant byte of an integer, so we need to do a byte swap.
-  *   Note that this is the opposite of when we typically do byte swapping due to the
-  *   oddity of running on a PPC.
-  */
 
 #include <assert.h>
 #include <ctype.h>
