30119b0e558a5f50d91e3d8adbe568eefd41b9ae
John Jackson <john.jackson@mirageinfinite>
2023-10-31 21:40:17 -0400

    more stuff

--- playground/gpio_control.cpp
+++ playground/gpio_control.cpp
@@ -1,30 +1,56 @@
 #include <pigpio.h>
 #include <iostream>
 
-const int inputPin = 22;   // BCM GPIO 6
-const int outputPin = 26;  // BCM GPIO 7
+const int inputPin = 22;    //PIN 15 interupt pin
+const int outputPin = 27;   //PIN 13 response to interupt pin
+const int togglePin = 17;   //PIN 11 fake clock/interupt agressor
 
-int main(void) {
+// Callback function to be called when the input pin changes state
+void inputCallback(int gpio, int level, uint32_t tick) {
+    // Set the output pin based on the input pin level
+    gpioWrite(outputPin, level);
+}
+
+int main() {
+    // Initialize pigpio library
     if (gpioInitialise() < 0) {
         std::cerr << "Failed to initialize pigpio" << std::endl;
         return 1;
     }
 
+    // Set GPIO modes
     gpioSetMode(inputPin, PI_INPUT);
     gpioSetMode(outputPin, PI_OUTPUT);
+    gpioSetMode(togglePin, PI_OUTPUT);
 
-    std::cout << "Starting loop..." << std::endl;
+    // Set pull-down resistor on the input pin
+    gpioSetPullUpDown(inputPin, PI_PUD_DOWN);
 
+    // Set the callback function for the input pin
+    gpioSetAlertFunc(inputPin, inputCallback);
+
+    // Set up for toggling the third pin at 120 Hz
+    const int toggleFrequency = 120;  // Toggle frequency in Hz
+    const uint32_t toggleInterval = 1000000 / toggleFrequency;  // Interval in microseconds
+    uint32_t lastToggleTime = gpioTick();  // Last toggle time
+
+    std::cout << "Starting loop, press CTRL+C to exit..." << std::endl;
+
+    // Main loop
     while (1) {
-        if (gpioRead(inputPin) == 1) {
-            gpioWrite(outputPin, 1);
-        } else {
-            gpioWrite(outputPin, 0);
+        uint32_t currentTime = gpioTick();
+        // Check if it's time to toggle the pin
+        if (currentTime - lastToggleTime >= toggleInterval) {
+            // Read the current state and write the inverse
+            gpioWrite(togglePin, !gpioRead(togglePin));
+            lastToggleTime = currentTime;
         }
 
-        time_sleep(0.01);  // Small delay to prevent bouncing
+        // Small delay to prevent 100% CPU utilization in the loop
+        gpioDelay(100);  // Delay for 1 millisecond
     }
 
+    // Cleanup the pigpio resources
     gpioTerminate();
     return 0;
 }
